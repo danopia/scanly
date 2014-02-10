f050007325b6bd9dd0b6eee7ac7cb975e129099d
Daniel Lamando <dan@danopia.net>
2014-02-10 00:50:24 -0500

    Move SANE code to a different file

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-var spawn = require('child_process').spawn;
-
-// list scanner devices
-var getDevices = function (callback) {
-  console.log('requesting a device list');
-  var proc = spawn('scanimage', ['-f', '%d%n%v%n%m%n%t%n%i%n--scanly--%n']);
-  
-  // buffer output
-  var output = '';
-  proc.stdout.on('data', function (chunk) {
-    output += chunk;
-  }).setEncoding('utf8');
-  
-  proc.on('close', function (code) {
-    if (code) {
-      console.log('device listing crashed with code', code);
-      callback(code);
-      return;
-    }
-    
-    // parse out our format
-    var list = output.split('\n--scanly--\n');
-    list.pop(); // drop the empty
-    
-    var devices = list.map(function (item) {
-      var data = item.split('\n');
-      return {
-        name: data[0],
-        vendor: data[1],
-        model: data[2],
-        type: data[3]};
-    });
-    
-    console.log('sane listed', devices.length, 'devices');
-    callback(null, devices);
-  });
-};
+var sane = require('./sane');
 
 // test code
-getDevices(function (err, devices) {
+sane.getDevices(function (err, devices) {
   console.log(devices);
 });
 
